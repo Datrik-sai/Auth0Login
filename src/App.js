04e071ced567007cd78a4ed8c3b006b4719ca1ac
Datrik Saikiran <datriksaikiran111@gmail.com>
2021-04-08 08:39:21 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import LoginButton from './Components/LoginButton';
+import LogoutButton from './Components/LogoutButton';
+import Profile from './Components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
+  );
+}
+
+export default App;
